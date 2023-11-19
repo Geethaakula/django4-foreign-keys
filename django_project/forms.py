ff753e2295e1254ee21a25da9fb8887585420023
Geetha <geethasyamakula@gmail.com>
2023-11-18 22:27:20 -0600

    add filters using forms.py

--- django_project/forms.py
+++ django_project/forms.py
@@ -2,6 +2,7 @@ from django.db.models.base import Model
 from django.forms import ModelForm, HiddenInput,ModelChoiceField,Select
 from .models import Input,Region
 from .widget import DateTimePickerInput
+from django import forms
 
 
 class TicketForm(ModelForm):
@@ -14,3 +15,29 @@ class TicketForm(ModelForm):
         }
     region = ModelChoiceField(queryset=Region.objects.all(),to_field_name='name',required=True,
                               widget=Select)
+    
+class DayForm(forms.Form):
+    DAYS_CHOICES = [
+        ('Monday', 'Monday'),
+        ('Tuesday', 'Tuesday'),
+        ('Wednesday', 'Wednesday'),
+        ('Thursday', 'Thursday'),
+        ('Friday', 'Friday'),
+        ('Saturday', 'Saturday'),
+        ('Sunday', 'Sunday'),
+    ]
+    REGION_CHOICES = [
+        ('A', 'A'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('D', 'D'),
+        ('E', 'E'),
+        ('F', 'F'),
+        ('G', 'G'),
+    ]
+
+    region = forms.ChoiceField(choices=REGION_CHOICES)
+
+    day = forms.ChoiceField(choices=DAYS_CHOICES)
+    
+    
--- django_project/views.py
+++ django_project/views.py
@@ -2,27 +2,29 @@ import datetime
 from django.views import View
 # from django_project.models import Region,Input
 from django.shortcuts import render
-from .forms import TicketForm 
+from .forms import TicketForm,DayForm
 from .models import Input,Region
+
+
 regions=list(Region.objects.all())
 
 # class Home(View):
 #     def get(self,request):
-        
+		
 #         return render(request,'index.html',{"regions": regions})
 
 #     def 
 
 #     def post(self,request):
 #         a = Region.objects.get(name=request.POST["location"])
-        
+		
 #         times = datetime.datetime.strptime(request.POST['geetha'], '%Y-%m-%dT%H:%M')
-        
+		
 #         ticketInfo = Input(time= times, address=request.POST["StreetAddress"],region= a, day = times.strftime("%A"))
-        
-        
+		
+		
 #         ticketInfo.save()
-        
+		
 #         return render(request, "index.html", {'regions': regions})
 
 class Home(View):
@@ -40,16 +42,16 @@ class Home(View):
 			form.save()
 		return render(request, 'index.html', {'regions':regions, 'form': TicketForm()})
 
-    
+	
 class History(View):
-    def get(self,request):
-        return render(request, 'history.html', {'regions': regions, 'tickets': []})  # An empty list initially
-    
-    def post(self, request):
-        region_name = request.POST["location"]
-        day_of_week = request.POST["day"]
-        b=list(Region.objects.filter(name=region_name))[0]
-        tickets = Input.objects.filter(region=b, day=day_of_week)
-        print(tickets)
-        return render(request, 'history.html', {'regions': list(regions), 'tickets': list(tickets)})
-        
+	def get(self,request):
+		return render(request, 'history.html', {'form': DayForm()})  # An empty list initially
+	
+	def post(self, request):
+		region = request.POST["region"]
+		day = request.POST["day"]
+		b=list(Region.objects.filter(name=region))[0]
+		tickets = Input.objects.filter(region=b, day=day)
+		print("tickets ",tickets)
+		return render(request, 'history.html', {'regions': list(regions), 'tickets': list(tickets), 'form': DayForm()})
+		
